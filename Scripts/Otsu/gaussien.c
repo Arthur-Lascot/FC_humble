0d03185d8c0524c23d5ce1e091d8c0c67e354266
Terence Degroote <terence.degroote2epita.com>
2021-11-17 10:15:53 +0100

    avec nvx fichier

---
+++ Scripts/Otsu/gaussien.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <SDL.h>
+#include <err.h>
+#include "SDL/SDL.h"
+#include "SDL/SDL_image.h"
+#include "base_function_on_pict.h"
+#include <assert.h>
+
+SDL_Surface* gaussien (SDL_Surface * image)
+{
+    for (int i = 0 ; i < image->w ; i++)
+    {
+        for (int j = 0 ; i < image->h ; i++)
+        {
+            for 
+        }
+    }
+}
+
+int main()
+{
+
+}
+
+
+
+
---
+++ Scripts/Otsu/gaussien.h
@@ -0,0 +1,11 @@
+#ifndef GAUSSIEN_H_
+#define GAUSSIEN_H_
+
+
+#include <stdlib.h>
+#include <SDL.h>
+
+SDL_Surface* gaussien(SDL_Surface* image); 
+
+#endif
+
